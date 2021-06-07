742ec0331da39a03f918acc9330b0a940e004aee
[Melisa San] <[melisasannn@gmail.com]>
2021-06-07 11:17:31 +0300

    ConsoleApplicationDemo

---
+++ ConsoleApplicationDemo/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApplicationDemo
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Isminizi girniz");
+            string name = Console.ReadLine();
+            Console.WriteLine("Soyadınızı girniz");
+            string surname = Console.ReadLine();
+            Console.WriteLine("Isminiz : " + name +"Soyisminiz" + surname);
+
+        }
+    }
+}
