22991210a1923a59e826d76d83fa74c7e46f999f
[Melisa San] <[melisasannn@gmail.com]>
2021-06-09 21:49:43 +0300

    AddDictionary

---
+++ Collections/DictionaryCollection/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DictionaryCollection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // System.Collections.Generic
+            //Dictionary<key,value>
+            Dictionary<int,string> kullanicilar = new Dictionary<int, string>();
+
+            kullanicilar.Add(10,"Melisa San");
+            kullanicilar.Add(15,"Serkan Altuntaş");
+            kullanicilar.Add(19,"Oğuzhan Kubilay Güzel");
+            kullanicilar.Add(24,"Barış Dilber");
+
+
+            //Dizinin elemanlarına erişim
+            Console.WriteLine("************Elemanlara Erişim*********");
+            Console.WriteLine(kullanicilar[10]);
+
+            foreach (var item in kullanicilar)
+            {
+                Console.WriteLine(item);
+            }
+
+            //Count
+            Console.WriteLine("Count: " + kullanicilar.Count);
+
+            //Contains
+            Console.WriteLine("Contains Key: " + kullanicilar.ContainsKey(15)); //returns true
+            Console.WriteLine("Contains Value: " + kullanicilar.ContainsValue("Serkan Altuntaş")); //returns true
+
+            //Remove
+            kullanicilar.Remove(19);
+            Console.WriteLine("Removed Dictionary");
+            foreach (var item in kullanicilar)
+            {
+                Console.WriteLine(item.Value);
+            }
+
+
+            //Keys
+            Console.WriteLine("**********Keys***********");
+
+            foreach (var item in kullanicilar)
+            {
+                Console.WriteLine(item.Key);
+            }
+
+
+
+            
+        
+
+
+
+        }
+    }
+}
